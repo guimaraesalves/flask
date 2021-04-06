084f993fcb569941e2a8b8e3cbd0ca501bad734e
Mateus Guimarães Alves <guimaraesalves45@gmail.com>
2021-04-06 19:54:41 -0300

    update estudo flask

--- PrettyPrinted/introduction_python_flask_templates/macros/macros.py
+++ PrettyPrinted/introduction_python_flask_templates/macros/macros.py
@@ -4,7 +4,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    myList = [293, 374, 943, 518, 192]
+    moreNumbers = [83398, 28739, 84379, 48983223, 298423, 47855, 3940843, 23483]
+    evenMoreNumbers = [192, 392332334585484]
+    return render_template('index.html', name='Guimarães', numbers=myList, moreNumbers=moreNumbers, evenMoreNumbers=evenMoreNumbers)
 
 if __name__ == '__main__':
     app.run(debug=True)
