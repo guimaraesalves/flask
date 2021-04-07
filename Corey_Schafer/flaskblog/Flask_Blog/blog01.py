022873be64014f21c552013c6bd510a0f273d3e0
Mateus Guimarães Alves <guimaraesalves45@gmail.com>
2021-04-07 20:16:24 -0300

    update tutorial flask

--- Corey_Schafer/flaskblog/Flask_Blog/blog01.py
+++ Corey_Schafer/flaskblog/Flask_Blog/blog01.py
@@ -1,14 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Mateus Guimarães Alves',
+        'title': 'Projeto 1 - Meu próprio Blog',
+        'content': 'Um blog para  o meu portifólio',
+        'date_posted': 'Abril 07, 2021'
+    },
+    {
+        'author': 'Mateus Guimarães Alves',
+        'title': 'Projeto 2 - Hank-Pessoa',
+        'content': 'Um Chatbot para conversar com o Poeta Fernando Pessoa ',
+        'date_posted': 'Abril 07, 2021'
+    },
+]
+
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
