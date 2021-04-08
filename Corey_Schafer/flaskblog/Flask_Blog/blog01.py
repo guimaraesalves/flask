9654045a54bb32f86a60852eedf6528c71da0242
Mateus Guimarães Alves <guimaraesalves45@gmail.com>
2021-04-08 00:29:13 -0300

    update flask

--- Corey_Schafer/flaskblog/Flask_Blog/blog01.py
+++ Corey_Schafer/flaskblog/Flask_Blog/blog01.py
@@ -1,45 +1,55 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
-
-app.config['SECRET_KEY'] = 'd95ba8a27b1f1fab4dd168953ff6603b'
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 posts = [
     {
-        'author': 'Mateus Guimarães Alves',
-        'title': 'Projeto 1 - Meu próprio Blog',
-        'content': 'Um blog para  o meu portifólio',
-        'date_posted': 'Abril 07, 2021'
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
     },
     {
-        'author': 'Mateus Guimarães Alves',
-        'title': 'Projeto 2 - Hank-Pessoa',
-        'content': 'Um Chatbot para conversar com o Poeta Fernando Pessoa ',
-        'date_posted': 'Abril 07, 2021'
-    },
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
 ]
 
 
-@app.route('/')
-@app.route('/home')
+@app.route("/")
+@app.route("/home")
 def home():
     return render_template('home.html', posts=posts)
 
-@app.route('/about')
+
+@app.route("/about")
 def about():
     return render_template('about.html', title='About')
 
-@app.route('/register')
+
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
-@app.route('/login')
+
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
-
 
 
 if __name__ == '__main__':
